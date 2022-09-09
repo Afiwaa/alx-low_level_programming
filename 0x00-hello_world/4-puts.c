f88350cb024cbe0ec0a90b44e817d90b1256cc6d
Afiwaa <carolkpeli@gamil.com>
2022-09-08 19:01:28 -0700

    Printing a string with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Printing a standard output using 'puts'
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
