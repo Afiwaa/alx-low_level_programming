ca8a4cc7781a61f9bda549865d38f730318f699e
Afiwaa <carolkpeli@gamil.com>
2022-09-10 19:13:10 -0700

    Printing upper and lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Printing both uppercases and lowercases
+ * Return: return 0
+ */
+int main(void)
+{
+	char m;
+	char p;
+	for (m = 'a'; m <= 'z'; m++)
+	{
+		putchar(m);
+	}
+	for (p = 'A'; p <= 'Z'; p++)
+	{
+		putchar(p);
+	}
+	putchar('\n');
+	return (0);
+}
+
