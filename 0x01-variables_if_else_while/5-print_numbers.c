c95a63f118f4169d14e417b4ede0e01dfac2a27e
Afiwaa <carolkpeli@gamil.com>
2022-09-10 19:39:17 -0700

    Print single numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single numbers
+ * Return: return 0
+ */
+int main(void)
+{
+	int k;
+	for (k = 0; k <= 9; k++)
+	{
+		printf("%d", k);
+	}
+	putchar(k);
+	return (0);
+}
+
