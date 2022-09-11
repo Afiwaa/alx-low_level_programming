12dc416bb5f5a252d5619031d1e9f44018dce086
Afiwaa <carolkpeli@gamil.com>
2022-09-10 19:28:41 -0700

    Lowercase with exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print in lowercase with exceptions
+ * Return: returns 0
+ */
+int main(void)
+{
+	char m;
+	for (m = 'a'; m <= 'z'; m++)
+	{
+		if (m != 'e' && m != 'q')
+			putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
+
