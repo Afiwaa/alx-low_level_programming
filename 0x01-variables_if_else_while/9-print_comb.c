f18c3cfcf4a785779bcd5b4ce1ab33dcd749a0c7
Afiwaa <carolkpeli@gamil.com>
2022-09-11 16:51:25 -0700

    Print all combination of single digit humbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Print all possible combination of single digit numbers separated by comma and whitespace
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = '0'; j <= '9'; j++)
+	{
+		putchar(j);
+		if (j != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
+
