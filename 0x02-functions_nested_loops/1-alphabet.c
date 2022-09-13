b1172f8ea537202da3cf2be9300aae1f268b4575
Afiwaa <carolkpeli@gamil.com>
2022-09-13 15:32:18 -0700

    Print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lowercase
+ * Returns: 0
+ */
+
+void print_alphabet(void)
+{
+	int k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		_putchar(k);
+	}
+	_putchar('\n');
+}
+
