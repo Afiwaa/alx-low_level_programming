88f59f4d296bdbdb7b747c24ee01e0c9bdd2351d
Afiwaa <carolkpeli@gamil.com>
2022-09-19 19:40:19 -0700

    Print half of a sting

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * puts_half - Print half a str
@@ -19,7 +18,7 @@ void puts_half(char *str)
 		n = (len + 1) / 2;
 
 	for (index = n; index < len; index++)
-		putchar(str[index]);
+		_putchar(str[index]);
 
-	putchar('\n');
+	_putchar('\n');
 }
