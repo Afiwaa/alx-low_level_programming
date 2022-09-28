af05631b1b47c885e950dc8d448595936dd8c07b
Afiwaa <carolkpeli@gamil.com>
2022-09-28 13:24:04 -0700

    Print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string pointer
+ */
+
+void _puts_recursion(char *s)
+{
+	int z;
+
+	if (*s != '\0')
+	{
+		_putchar (s[z]);
+		_puts_recursion (s + 1);
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+}
