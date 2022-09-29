952ba48527fa4a3a499d261428ab4a5dcf6a6eac
Afiwaa <carolkpeli@gamil.com>
2022-09-29 01:22:18 -0700

    Find square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * find_sqrt - find square root 
+ * @n: number
+ * @m: probable square root
+ * Return: squareroot
+ */
+
+int find_sqrt(int n, int g)
+{
+	if (g * g == n)
+	{
+		return (g);
+	}
+	else if (g * g > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (find_sqrt(n, g + 1));
+	}
+}
+
+/**
+ * _sqrt_recursion - calls find_sqrt
+ * @n: number
+ * Return: squareroot
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (find_sqrt(n, 0));
+}
+
